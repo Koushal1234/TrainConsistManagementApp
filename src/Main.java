1ee422c2028707d218dd21863d30b604d24b8b76
Koushal1234 <koushedupulapati@gmail.com>
2026-04-01 14:29:59 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class UC2 {
     public static void main(String[] args) {
-        System.out.println("=== Train Consist Management App ===");
 
-        List<String> train = new ArrayList<>();
+        List<String> bogies = new ArrayList<>();
 
-        System.out.println("Initial Bogie Count: " + train.size());
+        bogies.add("Sleeper");
+        bogies.add("AC Chair");
+        bogies.add("First Class");
+
+        System.out.println("After Adding: " + bogies);
+
+        bogies.remove("AC Chair");
+
+        System.out.println("After Removal: " + bogies);
+
+        System.out.println("Contains Sleeper: " + bogies.contains("Sleeper"));
     }
 }
